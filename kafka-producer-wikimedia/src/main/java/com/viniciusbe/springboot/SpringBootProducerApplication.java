019e98e59ed896043ab802e637fec954d72fc2ac
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-07-11 14:40:32 -0300

    Initial commit

---
+++ kafka-producer-wikimedia/src/main/java/com/viniciusbe/springboot/SpringBootProducerApplication.java
@@ -0,0 +1,24 @@
+package com.viniciusbe.springboot;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProducerApplication implements CommandLineRunner {
+
+    public SpringBootProducerApplication(WikimediaChangesProducer wikimediaChangesProducer) {
+        this.wikimediaChangesProducer = wikimediaChangesProducer;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    private final WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
+    }
+}
